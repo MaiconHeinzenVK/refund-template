47967e85a6930a35de28ba08bfdc15708a83fa2f
MaiconHeinzen <m41conv1k1ng@gmail.com>
2024-10-19 21:46:56 -0300

    amount accepts only letter

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//Selecionar elementos do formulario
+const amount = document.getElementById("amount")
+
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, "")
+  amount.value = value
+}
