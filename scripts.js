a8a62da6d519ed3e087376c7844f46e7201d7a8f
MaiconHeinzen <m41conv1k1ng@gmail.com>
2024-10-20 15:17:23 -0300

    Creating a new expense object

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 //Selecionar elementos do formulario
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "")
@@ -16,4 +20,19 @@ function formatCurrencyBrl(value) {
   })
 
   return value
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+
+  console.log(newExpense)
 }
